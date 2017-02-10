bd491db3073e6ffdb40fee687e17218766631f6b
Podorozhny <rp31@txstate.edu>
2017-02-09 18:15:38 -0600

    sensors added

--- src/model/SensorList.java
+++ src/model/SensorList.java
@@ -8,5 +8,7 @@ public class SensorList {
 		System.out.println("2_RP17 Sensor 3");
 		System.out.println("2_RP17 Sensor 4");
 		System.out.println("2_RP17 Sensor 5");
+		System.out.println("2_RP17 Sensor 6");
+		System.out.println("2_RP17 Sensor 7");
 		}
 }
